43e82b6b6d8ed610389eea5423688caa7be3e081
dani21123 <danidor59@gmail.com>
2019-06-21 17:16:03 +0300

    Initial commit

---
+++ app/src/main/java/com/example/ex4/Joystick.java
@@ -0,0 +1,29 @@
+package com.example.ex4;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Joystick extends View {
+
+
+
+    public Joystick(Context context) {
+        super(context);
+    }
+
+
+    /*
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+        String ip = intent.getStringExtra("ip");
+        String port = intent.getStringExtra("port");
+
+        setContentView(R.layout.activity_joystick);
+    }
+    */
+}
---
+++ app/src/main/java/com/example/ex4/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.ex4;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void buttonConnect(View view) {
+
+        EditText ip =
+                (EditText)findViewById(R.id.editIP);
+        EditText port =
+                (EditText)findViewById(R.id.editPort);
+        Intent intent = new Intent(this,
+                Joystick.class);
+        intent.putExtra("ip", String.valueOf(ip));
+        intent.putExtra("port", String.valueOf(port));
+        startActivity(intent);
+    }
+
+}
